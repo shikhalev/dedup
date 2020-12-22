841cf86aab840666c1fe9e580590913adab86805
Ivan Shikhalev <shikhalev@gmail.com>
2020-12-23 03:28:36 +0500

    Ср 23 дек 2020 03:28:36 +05

--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,34 @@
-use std::path::PathBuf;
+use std::{fs::Metadata, path::PathBuf};
+
+use crate::logger;
+
+fn process_dir(path: &PathBuf, md: &Metadata) {
+  todo!();
+}
+
+fn process_file(path: &PathBuf, md: &Metadata) {
+  todo!();
+}
+
+fn process_symlink(path: &PathBuf, md: &Metadata) {
+  logger::file(&path.to_string_lossy());
+  todo!();
+}
 
 pub fn process_path(path: &PathBuf) {
   dbg!(&path);
-  // todo!();
+  match path.metadata() {
+    Ok(md) => {
+      dbg!(&md);
+      let ft = md.file_type();
+      if ft.is_symlink() {
+        process_symlink(&path, &md);
+      } else if ft.is_dir() {
+        process_dir(&path, &md);
+      } else if ft.is_file() {
+        process_file(&path, &md);
+      }
+    }
+    Err(e) => logger::error(&e.to_string()),
+  }
 }
